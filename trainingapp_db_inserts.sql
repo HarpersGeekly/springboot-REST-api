705ce71da2d7fa28b7fa16865bccb7c6fc79f57d
ryan.c.harper <ryan.c.harper@accenturefederal.com>
2022-04-01 16:39:09 -0500

    seeder

---
+++ trainingapp_db_inserts.sql
@@ -0,0 +1,30 @@
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (1, 'testing comment', '2022-03-04 12:36:18.359000', false, 15, 1, true);
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (2, 'hello world', '2022-03-04 12:36:55.574000', false, 15, 1, false);
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (3, 'what', '2022-03-04 13:41:46.288000', false, 15, 1, false);
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (4, 'test', '2022-03-04 14:34:52.793000', false, 15, 1, false);
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (5, 'test', '2022-03-04 14:35:44.777000', false, 15, 1, true);
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (6, 'test1', '2022-03-04 14:50:21.699000', false, 15, 1, false);
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (7, 'commenting', '2022-03-04 15:39:47.939000', false, 15, 1, false);
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (8, 'tadsfadfs', '2022-03-04 15:46:33.368000', false, 15, 1, true);
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (9, 'commtn', '2022-03-04 15:47:17.960000', false, 14, 1, false);
+INSERT INTO trainingapp_db.comments (id, body, date, hasBeenDeleted, post_id, user_id, has_been_deleted) VALUES (10, 'test', '2022-03-10 17:21:52.050000', false, 15, 1, false);
+INSERT INTO trainingapp_db.posts (id, body, created_date, leadImage, subtitle, title, user_id, lead_image) VALUES (1, 'hello word', '2020-01-22 16:04:21', null, 'yep sdasfasf', 'HelloWorld', 1, null);
+INSERT INTO trainingapp_db.posts (id, body, created_date, leadImage, subtitle, title, user_id, lead_image) VALUES (3, 'third', '2021-11-08 14:22:42', null, 'three', 'three', 1, '');
+INSERT INTO trainingapp_db.posts (id, body, created_date, leadImage, subtitle, title, user_id, lead_image) VALUES (13, 'asdfasdf  adslfa;lksdjflk; as asdf;lajsdfklakdf;ajldf', '2022-02-07 16:45:46.599000', null, 'asdfasdfasdfasdfasd', 'asdasdfasdf', 1, null);
+INSERT INTO trainingapp_db.posts (id, body, created_date, leadImage, subtitle, title, user_id, lead_image) VALUES (14, 'asdasdfasdfadsf asdfasdfasdf', '2022-02-07 16:56:33.970000', null, 'asdfasdfasdf', 'user2''s post', 2, null);
+INSERT INTO trainingapp_db.posts (id, body, created_date, leadImage, subtitle, title, user_id, lead_image) VALUES (15, 'What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "L', '2022-02-17 16:01:42.829000', null, 'adslkjs;ldflk;ajsf ', 'Cyberpunk 2077 is back?', 1, '![](https://www.cyberpunk.net/build/images/social-thumbnail-en-ddcf4d23.jpg)');
+INSERT INTO trainingapp_db.posts_votes (post_id, user_id, type) VALUES (15, 1, 1);
+INSERT INTO trainingapp_db.posts_votes (post_id, user_id, type) VALUES (15, 2, 1);
+INSERT INTO trainingapp_db.users (id, bio, joined_date, email, password, username) VALUES (1, null, '2019-10-31 15:28:25', 'user1@email.com', 'password', 'user1');
+INSERT INTO trainingapp_db.users (id, bio, joined_date, email, password, username) VALUES (2, null, '2021-11-10 12:27:44', 'user2@email.com', 'password', 'user2');
+INSERT INTO trainingapp_db.users (id, bio, joined_date, email, password, username) VALUES (3, null, '2022-02-14 14:34:11.753000', 'user3@email.com', 'password', 'user3');
+INSERT INTO trainingapp_db.users (id, bio, joined_date, email, password, username) VALUES (4, null, '2022-02-14 14:39:25.171000', 'user4@email.com', 'password', 'user4');
+INSERT INTO trainingapp_db.users (id, bio, joined_date, email, password, username) VALUES (5, null, '2022-02-14 14:40:24.642000', 'user5@email.com', 'password', 'user5');
